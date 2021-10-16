a9c64b0bbf7eb0c65170f49d7708dc8a78882cc0
RITESHKHODIYAR <89767764+RITESHKHODIYAR@users.noreply.github.com>
2021-10-16 10:52:17 +0530

    Add files via upload

---
+++ MAVENPROJECT/src/main/java/MAVN/MAVENPROJECT/Mavenfirstprog.java
@@ -0,0 +1,12 @@
+package MAVN.MAVENPROJECT;
+
+import org.testng.annotations.Test;
+
+public class Mavenfirstprog {
+
+	@Test
+	public void verify() {
+		System.out.println("Runnig from maven");
+	}
+
+}
---
+++ MAVENPROJECT/src/main/java/MAVN/MAVENPROJECT/XML_OR.java
@@ -0,0 +1,39 @@
+package MAVN.MAVENPROJECT;
+
+import java.io.*;		
+import java.util.*;		
+import org.dom4j.*;		
+import org.dom4j.io.SAXReader;		
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.chrome.ChromeDriver;		
+
+public class XML_OR {				
+
+public static void main(String[] args) throws DocumentException {										
+// Creating WebDriver Instance		
+    WebDriver driver;			
+    System.setProperty("webdriver.chrome.driver","c://worksoft//chromedriver//chromedriver.exe");					
+    driver = new ChromeDriver();					
+    driver.get("http://demo.guru99.com/test/guru99home/");					
+    driver.manage().window().maximize();			
+// Reading XML File    		
+    File inputFile = new File(System.getProperty("user.dir") +"\\src\\Excel"+"\\xmlor.xml");									
+    SAXReader saxReader = new SAXReader();					
+    Document document = saxReader.read(inputFile);							
+    String mobileTesting = document.selectSingleNode("//menu/mobiletesting").getText();							
+    String emailTextBox = document.selectSingleNode("//menu/email").getText();							
+    String signUpButton = document.selectSingleNode("//menu/signup").getText();			
+
+
+    System.out.println(mobileTesting+"|"+emailTextBox+"|"+signUpButton);
+    
+//Navigating to Mobile Testing and back		
+    driver.findElement(By.xpath(mobileTesting)).click();					
+    driver.navigate().back();			
+//Entering Form Data		
+driver.findElement(By.id(emailTextBox)).sendKeys("testguru99@gmail.com");						
+driver.findElement(By.id(signUpButton)).click();				
+
+	}		
+}
